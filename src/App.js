227dce3dbe34466697ba1d69cacdadf377c73448
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-16 22:40:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import AddNumbers from './AddNumbers';
 import { useState } from 'react';
 
 function App() {
@@ -24,47 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-function App1() {
-  return (<p>
-          Test return funtion paragraph
-        </p>
-    );
-}
-
-export default function adresses() {
-  const [address, setAddress] = useState('');
-  const [balance, setBalance] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=YOUR_API_KEY`);
-      const data = await response.json();
-      const balanceInEther = parseFloat(data.result) / 1000000000000000000;
-      setBalance(balanceInEther);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    setAddress(e.target.value);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Ethereum Address:
-          <input type="text" value={address} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Check Balance</button>
-      </form>
-      {balance && <p>Balance: {balance} ETH</p>}
-    </div>
-  );
-}
-
