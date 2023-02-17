073a3cb8bcf6fc8d04a02e54f2831fd1486a7442
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-17 14:06:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,3 +24,24 @@ function App() {
 
 export default App;
 
+function App1() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Enjoy !!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >TOMASZ, Learn React to be a professional fullstack dev.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App1;
