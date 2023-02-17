48f9f77220d8180281553faf77897c9308318ffc
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-17 14:05:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App1 from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 import { useState } from 'react';
@@ -10,7 +9,6 @@ import { useState } from 'react';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-   <App1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
