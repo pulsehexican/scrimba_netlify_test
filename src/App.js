a388b4b0f22f5b3c1c9031eeaf5018756b86624a
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-17 13:40:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >TOmasz, Learn React to be a professional fullstack dev.
+        >TOMASZ, Learn React to be a professional fullstack dev.
         </a>
       </header>
     </div>
@@ -24,24 +24,3 @@ function App() {
 
 export default App;
 
-function App1() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> save to reload !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >testing dowiesz sie w swoim czasie
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
